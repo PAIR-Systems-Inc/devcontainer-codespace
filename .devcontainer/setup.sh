f626f21e16afd0e5ba981b8dff0339a164575ac2
mariamahmad8 <157257261+mariamahmad8@users.noreply.github.com>
2025-08-22 18:55:00 -0700

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -77,13 +77,14 @@ if command -v javac >/dev/null 2>&1; then
     cd "$JAVA_APP_DIR"
 
     # Change it to look like this
-    yes | gradle -q init \
+    gradle -q init \
       --type java-application \
       --dsl kotlin \
       --test-framework junit \
       --project-name "$JAVA_APP_DIR" \
       --package com.pairsystems.goodmem.sample \
       --java-version 21
+
 
 
 
